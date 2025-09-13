5ed5f80a33978e805faec696d2b9ee59d9bddf08
Ado <minexdt@gmail.com>
2025-09-12 20:06:27 -0600

    Actualizar _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -12,14 +12,32 @@ const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "stat
 let chat = global.db.data.chats[m.chat]
 let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'
-let nombre, foto, edit, newlink, status, admingp, noadmingp
-nombre = `❀ ${usuario} Ha cambiado el nombre del grupo.\n\n> ✦ Ahora el grupo se llama:\n> *${m.messageStubParameters[0]}*.`
-foto = `❀ Se ha cambiado la imagen del grupo.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
-edit = `❀ ${usuario} Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo.`
-newlink = `❀ El enlace del grupo ha sido restablecido.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
-status = `❀ El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado*' : '*abierto*'} Por ${usuario}\n\n> ✦ Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje.`
-admingp = `❀ @${m.messageStubParameters[0].split`@`[0]} Ahora es admin del grupo.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
-noadmingp = `❀ @${m.messageStubParameters[0].split`@`[0]} Deja de ser admin del grupo.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
+
+let nombre = `> *_Cambio de nombre del grupo_*
+✩ *Acción*: ${usuario}
+✩ *Nuevo nombre*: *${m.messageStubParameters[0]}*`
+
+let foto = `> *_Cambio de imagen del grupo_*
+✩ *Acción*: ${usuario}`
+
+let edit = `> *_Permisos de configuración_*
+✩ *Acción*: ${usuario}
+✩ *Ahora pueden editar*: ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'}`
+
+let newlink = `> *_Enlace del grupo restablecido_*
+✩ *Acción*: ${usuario}`
+
+let status = `> *_Estado del grupo_*
+✩ *Acción*: ${usuario}
+✩ *Ahora*: ${m.messageStubParameters[0] == 'on' ? '*solo admins pueden enviar*' : '*todos pueden enviar*'}`
+
+let admingp = `> *_Nuevo administrador_*
+✩ *Usuario*: @${m.messageStubParameters[0].split`@`[0]}
+✩ *Acción*: ${usuario}`
+
+let noadmingp = `> *_Admin removido_*
+✩ *Usuario*: @${m.messageStubParameters[0].split`@`[0]}
+✩ *Acción*: ${usuario}`
 
 if (chat.detect && m.messageStubType == 2) {
 const uniqid = (m.isGroup ? m.chat : m.sender)
@@ -29,9 +47,9 @@ if (file.includes(uniqid)) {
 await fs.unlink(path.join(sessionPath, file))
 console.log(`${chalk.yellow.bold('[ Archivo Eliminado ]')} ${chalk.greenBright(`'${file}'`)}\n` +
 `${chalk.blue('(Session PreKey)')} ${chalk.redBright('que provoca el "undefined" en el chat')}`
-)}}
+)}}}
 
-} else if (chat.detect && m.messageStubType == 21) {
+else if (chat.detect && m.messageStubType == 21) {
 await this.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })  
 } else if (chat.detect && m.messageStubType == 22) {
 await this.sendMessage(m.chat, { image: { url: pp }, caption: foto, mentions: [m.sender] }, { quoted: fkontak })
@@ -47,9 +65,10 @@ await this.sendMessage(m.chat, { text: admingp, mentions: [`${m.sender}`,`${m.me
 await this.sendMessage(m.chat, { text: noadmingp, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })
 } else {
 if (m.messageStubType == 2) return
-console.log({messageStubType: m.messageStubType,
+console.log({
+messageStubType: m.messageStubType,
 messageStubParameters: m.messageStubParameters,
 type: WAMessageStubType[m.messageStubType], 
-})
-}}
+})}}
+
 export default handler
