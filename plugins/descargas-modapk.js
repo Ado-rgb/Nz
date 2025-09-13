1dca84225c47514a9b414d03b728eed5709bdcc5
Ado <minexdt@gmail.com>
2025-09-12 20:14:14 -0600

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -1,30 +1,47 @@
 import { search, download } from 'aptoide-scraper'
 
-var handler = async (m, {conn, usedPrefix, command, text}) => {
-if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el nombre de la apk para descargarlo.`, m)
-try {
-await m.react(rwait)
-conn.reply(m.chat, `${emoji} Descargando su aplicación, espere un momento...`, m)
-let searchA = await search(text)
-let data5 = await download(searchA[0].id)
-let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
-txt += `☁️ *Nombre* : ${data5.name}\n`
-txt += `🔖 *Package* : ${data5.package}\n`
-txt += `🚩 *Update* : ${data5.lastup}\n`
-txt += `⚖ *Peso* :  ${data5.size}`
-await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m) 
-await m.react(done)  
-if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, `${emoji2} El archivo es demaciado pesado.`, m)}
-await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
-} catch {
-return conn.reply(m.chat, `${msm} Ocurrió un fallo...`, m)}}
+var handler = async (m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, `✩ Por favor, ingrese el nombre de la apk para descargarla.`, m)
+    try {
+        await m.react("🕓") 
+
+        let searchA = await search(text)
+        let data5 = await download(searchA[0].id)
+
+        let txt = `> *_APT - DESCARGAS_*\n\n`
+        txt += `✩ *Nombre*: ${data5.name}\n`
+        txt += `✩ *Package*: ${data5.package}\n`
+        txt += `✩ *Ultima Actualizacion*: ${data5.lastup}\n`
+        txt += `✩ *Peso*: ${data5.size}`
+
+        await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m)
+
+        if (data5.size.includes('GB') || parseFloat(data5.size.replace(' MB', '')) > 999) {
+            await m.react(error) 
+            return conn.reply(m.chat, `✩ El archivo es demasiado pesado, supera el límite permitido de descarga.`, m)
+        }
+
+        await conn.sendMessage(
+            m.chat,
+            {
+                document: { url: data5.dllink },
+                mimetype: 'application/vnd.android.package-archive',
+                fileName: data5.name + '.apk',
+                caption: `> ✩ Descarga completa.`
+            },
+            { quoted: fkontak }
+        )
+
+        await m.react(done) 
+    } catch {
+        await m.react(error)
+        return conn.reply(m.chat, `✩ Ocurrió un fallo...`, m)
+    }
+}
 
 handler.tags = ['descargas']
 handler.help = ['apkmod']
 handler.command = ['apk', 'modapk', 'aptoide']
-handler.group = true;
-handler.register = true;
-handler.coin = 5;
-
+handler.group = true
+handler.register = true
 export default handler
